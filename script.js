b02f5bd2a43fef195f3a06385ec29893c65a91ab
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-07-08 15:56:36 -0400

    non-working loop...just saving for ref

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// # password generator (office hours rundown)
-//   event listener
-//   - on click some button
-//   - someElem.addEventListener("click", function() {})
-//   prompt user for input
-//     1. num of char
-//      - validate user input for 8 > num < 129
-//      - validate for a number
-//     - bunch of booleans
-//   approaches for possible password chars
-//     1. uild array for potential chars
-//       booleans if branches to add char
-//     2. acii ref for char
-//       - use random num for range within ascii char code for upper or lower, etc
-//   loop through length of pass to add a char to password
-//   finally displaying password
-
-
 // Arrays for all possible characters
 const lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -32,52 +14,52 @@ const numberPass = document.querySelector("#number");
 const specialPass = document.querySelector("#special");
 const lengthPass = document.querySelector("#length");
 
-// Create an array for possible password
-let possiblePass = [];
+function generatePassword() {
 
-// Create an array for generated password
-let finalPass =[];
+    // Create an array for possible password
+    let possiblePass = [];
 
-// Add checked options to the possible password array
-if (lowerPass.checked) {
-    possiblePass = possiblePass.concat(lowerCase);
-}
-if (upperPass.checked) {
-    possiblePass = possiblePass.concat(upperCase);
-}
-if (numberPass.checked) {
-    possiblePass = possiblePass.concat(numbers);
-}
-if (specialPass.checked) {
-    possiblePass = possiblePass.concat(specialChars);
-}
+    // Create an array for generated password
+    let finalPass = [];
 
-
-// Need to validate the password length
-// Need to loop through possiblePass to create finalPass based on lengthPass
-
-
-if (lengthPass < 8 || lengthPass > 128) {
-    alert ("Please select a number between 8 and 128.");
-}
-else {
-    for (var i = 0; i < lengthPass; i++) {
-        var num = Math.floor(Math.random() * lengthPass);
+    // Add checked options to the possible password array
+    if (lowerPass.checked) {
+        possiblePass = possiblePass.concat(lowerCase);
     }
+    if (upperPass.checked) {
+        possiblePass = possiblePass.concat(upperCase);
+    }
+    if (numberPass.checked) {
+        possiblePass = possiblePass.concat(numbers);
+    }
+    if (specialPass.checked) {
+        possiblePass = possiblePass.concat(specialChars);
+    }
+    console.log(possiblePass);
+    
+
+    // // Validate the password length & loop to create finalPass
+    // if (lengthPass < 8 || lengthPass > 128) {
+    //     alert("Please select a number between 8 and 128.");
+    //     return "";
+    // }
+    // else if{
+    //     for (let i = 0; i < lengthPass; i++) {
+    //         let num = Math.floor(Math.random() * lengthPass);
+    //         return password;
+    //     }
+    // }
+    // else {
+    //     alert("Select at least one password criteria box to continue.");
+    //     return "";
+    // }
+
+
+    // Still trying to figure out how to loop through 
+    // and arrange if/else statements....
+    // Need to do more research
+    
 }
-
-
-//Borrowed from previous activity to recall looping 
-for (var i = 0; i < 10; i++) {
-    // Generate a random number between 1 and 10
-    // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
-    var num = Math.floor(Math.random() * 10) + 1;
-
-    // Display in console
-    console.log(num);
-  }
-
-
 
 
 
@@ -86,9 +68,9 @@ for (var i = 0; i < 10; i++) {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
 
 }
