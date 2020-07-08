d4260c640f7c9034a59ea0f71e47e86fcec3871f
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-07-08 10:11:45 -0400

    copied class notes to file for reference

---
+++ script.js
@@ -0,0 +1,16 @@
+// # password generator (office hours rundown)
+//   event listener
+//   - on click some button
+//   - someElem.addEventListener("click", function() {})
+//   prompt user for input
+//     1. num of char
+//      - validate user input for 8 > num < 129
+//      - validate for a number
+//     - bunch of booleans
+//   approaches for possible password chars
+//     1. uild array for potential chars
+//       booleans if branches to add char
+//     2. acii ref for char
+//       - use random num for range within ascii char code for upper or lower, etc
+//   loop through length of pass to add a char to password
+//   finally displaying password
