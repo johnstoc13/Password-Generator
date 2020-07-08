c8e2ae4a8ce95ce306f85b0d18b1d3c58f10d394
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-07-08 12:16:38 -0400

    added homework js code to work off of

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz".split(" ");
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(" ");
 const numbers = "0123456789".split(" ");
 const specialChars = `!"#$%&'()*+,-./:;<=>?@[]^_{|}~`.split(" ");
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
