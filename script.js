373de1c1d77b972d01cf62a8345aee6c23784101
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-07-09 22:49:35 -0400

    optimized js so function stops if error reached

--- script.js
+++ script.js
@@ -16,15 +16,27 @@ const specialPass = document.querySelector("#special");
 // Password Generator
 function generatePassword() {
 
+    // Obtain password length in order to validate
+    // Cited: https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript
+    let pwLength = document.querySelector("#length").value;
+
+    // Validates password length
+    // Cited: https://www.w3schools.com/js/tryit.asp?filename=tryjs_validation_number
+    if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
+        alert("Please select a number between 8 and 128.");
+        return "";
+    }
+    // Validates that at least one box is checked
+    if (!lowerPass.checked && !upperPass.checked && !numberPass.checked && !specialPass.checked)  {
+        alert("Select at least one password criteria box to continue.");
+        return "";
+    }
+
     // Create an array for possible password
     let possiblePass = [];
 
     // Create an array for newly created password
     let createdPass = [];
-
-    // Obtain password length in order to validate
-    // Cited: https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript
-    let pwLength = document.querySelector("#length").value;
 
     // Add checked options to the possible password array
     if (lowerPass.checked) {
@@ -44,19 +56,7 @@ function generatePassword() {
     for(i = 0; i < pwLength; i++) {
         createdPass.push(possiblePass[Math.floor(Math.random() * possiblePass.length)])
     }
-
-    // Validates password length
-    // Cited: https://www.w3schools.com/js/tryit.asp?filename=tryjs_validation_number
-    if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
-        alert("Please select a number between 8 and 128.");
-        return "";
-    }
-    // Validates that at least one box is checked
-    if (!lowerPass.checked && !upperPass.checked && !numberPass.checked && !specialPass.checked)  {
-        alert("Select at least one password criteria box to continue.");
-        return "";
-    }
-
+    
     // Takes array of created password and joins to make string of final password
     let finalPass = createdPass.join("");
     return finalPass;
